115c90c4f9a4b56137c9c2397f76786f957fd40d
triniskye <trini.skye@outlook.com>
2022-01-24 21:28:21 +1000

    all components are working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,36 @@
-import React from "react";
-import {robots} from "../data/robots.js";
+import React, {useState} from "react";
+import {robots as Robots} from "../data/robots.js";
 import CardList from './CardList.jsx';
 import SearchBox from "./SearchBox.jsx";
 
 const App=()=>{
+
+    let [robots, setRobots] = useState(Robots);
+    let [resultError, setResultError] = useState(false);
+    function onSearchChange(e){
+        let searchTerm = e.target.value
+        const filteredRobots = robots.filter(r=>{
+            return r.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+        if (filteredRobots.length > 0 && searchTerm.length > 0){
+            setRobots(filteredRobots);
+            setResultError(false);
+        }
+        else if(filteredRobots.length < 1 && searchTerm.length > 0){
+            setResultError(true);
+            setRobots(Robots);
+        }
+        else{
+            setRobots(Robots);
+            setResultError(false);
+        }
+        
+
+    }
     return(
         <div className="tc">
             <h1>RoboFriends!</h1>
-            <SearchBox/>
+            <SearchBox error = {resultError} change={onSearchChange}/>
             <CardList robots = {robots}/>
         </div>
     )
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const SearchBox=()=>{
+const SearchBox=({change, error})=>{
+
     return(
         <div>
             <input className="pa3 ba b--green bg-lightest-blue mv3" 
             type="search" 
+            onChange={change}
             placeholder="Search Robots"/>
+            {error? <p style={{color:"red"}}>No results were found.</p> : <p></p>}
         </div>
     )
 }  
